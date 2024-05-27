8ea23d0e47c12cb7ab4a5452b098f48cd45b1ba2
PauloEwerson <ewersonmrt@gmail.com>
2024-05-27 13:49:16 -0300

    fix: Insere AuthProvider no index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 import "./styles/tailwind.css";
 import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return <Routes />; 
 }
 
 export default App;
